a3e493857a071787e537dafa95f07894f460e51a
Erwin_Susanto <erwin.susanto@erajaya.com>
2023-04-18 10:59:06 +0700

    20230418 Add model for egiftcard

---
+++ model/giftcard_detail.go
@@ -0,0 +1,15 @@
+package model
+
+type GiftcardDetail struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Card      []Card `json:"cards"`
+}
+
+type Card struct {
+	CardNumber  string `json:"card_number"`
+	CardPin     string `json:"card_pin"`
+	Denom       string `json:"denom"`
+	ExpiryDate  string `json:"expiry_date"`
+	OrderNumber string `json:"order_number"`
+}
